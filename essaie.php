d9ce3c587d37124575d1270f02eccc0af73f7f3b
uvsq22300928 <144265267+uvsq22300928@users.noreply.github.com>
2023-11-22 01:13:29 +0100

    Add files via upload

---
+++ essaie.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html> 
+<html>
+<head>
+  <img style="width:428px" src="https://raw.githubusercontent.com/Thibzzz04/test/efadc04902c4c6b5aa399f88def60b916196c65f/Capture%20d'%C3%A9cran%202023-10-25%20105748.png" />
+    <title>TagJobs</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 20px;
+            text-align: center;
+        }
+
+        h1 {
+            margin: 0;
+        }
+
+        .container {
+            width: 80%;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .job-listing {
+            margin-bottom: 20px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            background-color: #f9f9f9;
+        }
+
+        .job-title {
+            font-size: 1.2em;
+            font-weight: bold;
+        }
+
+        .job-company {
+            color: #777;
+        }
+
+        .job-location {
+            color: #777;
+        }
+
+        .job-description {
+            margin-top: 10px;
+        }
+
+        .apply-button {
+            background-color: #007BFF;
+            color: #fff;
+            padding: 5px 10px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .apply-button:hover {
+            background-color: #0056b3;
+        }
+    </style>
+<html>
+<head>
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 20px;
+            text-align: center;
+        }
+
+        h1 {
+            margin: 0;
+        }
+
+        .container {
+            width: 80%;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .search-box {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        .search-input {
+            padding: 10px;
+            width: 60%;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            font-size: 1em;
+        }
+
+        .search-button {
+            background-color: #007BFF;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            font-size: 1em;
+            cursor: pointer;
+        }
+
+        .search-button:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+<body>
+    </header>
+
+    <div class="container">
+        <div class="search-box">
+            <input class="search-input" type="text" placeholder="Rechercher un emploi...">
+            <button class="search-button">Rechercher</button>
+        </div>
+
+    </div>
+</body>
+</html>
+    <div class="container">
+        <div class="job-listing">
+            <div class="job-title">Formateur Web</div>
+            <div class="job-company">ANFAMA CONSULTING </div>
+            <div class="job-location">Lille, France</div>
+            <div class="job-description">
+                Nous recherchons un Formateur Web experimente pour rejoindre notre equipe. 
+            </div>
+            <button class="apply-button" onclick="window.location.href='/page3.html';">Postuler</button>
+        </div>
+        <div class="job-listing">
+            <div class="job-title">Designer UX/UI</div>
+            <div class="job-company">Design Studio</div>
+            <div class="job-location">Paris, France</div>
+            <div class="job-description">
+                Nous cherchons un designer UX/UI creatif et talentueux pour notre studio de design.
+            </div>
+            <button class="apply-button" onclick="window.location.href='/page2.html';">Postuler</button>
+        </div>
+    </div>
+</body>
+</html>
+<!DOCTYPE html>
+<html>
+  </html>
+  <div class="container">
+      <div class="job-listing">
+          <div class="job-title">Webmaster </div>
+          <div class="job-company">ANFAMA CONSULTING </div>
+          <div class="job-location">Lille, France</div>
+          <div class="job-description">
+              Nous recherchons un Webmaster experimente pour rejoindre notre equipe. 
+          </div>
+          <button class="apply-button" onclick="window.location.href='/page4.html';">Postuler</button>
+      </div>
+      <div class="job-listing">
+          <div class="job-title">Specialiste en referencement </div>
+          <div class="job-company">Design Studio</div>
+          <div class="job-location">Paris, France</div>
+          <div class="job-description">
+              Nous cherchons Specialiste en referencement et talentueux pour notre studio de design.
+          </div>
+          <button class="apply-button" onclick="window.location.href='/page5.html';">Postuler</button>
+      </div>
+  </div>
+</body>
+</html>
+<!DOCTYPE html>
+<html>
+  </html>
+  <div class="container">
+      <div class="job-listing">
+          <div class="job-title">Gestionnaire de contenu</div>
+          <div class="job-company">ANFAMA CONSULTING </div>
+          <div class="job-location">Lille, France</div>
+          <div class="job-description">
+              Nous recherchons un Gestionnaire de contenu pour rejoindre notre equipe. 
+          </div>
+          <button class="apply-button" onclick="window.location.href='/page6.html';">Postuler</button>
+      </div>
+      <div class="job-listing">
+          <div class="job-title">Architecte d'information</div>
+          <div class="job-company">Design Studio</div>
+          <div class="job-location">Paris, France</div>
+          <div class="job-description">
+              Nous cherchons un Architecte d'information et talentueux pour notre studio de design.
+          </div>
+          <button class="apply-button" onclick="window.location.href='/page7.html';">Postuler</button>
+      </div>
+  </div>
+</body>
+</html>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <footer>
+        <div class="footer-links">
+            <ul>
+                <li><a href="/accueil">Accueil</a></li>
+                <li><a href="/emplois">Offres d'emploi</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="/a-propos">A propos de nous</a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
